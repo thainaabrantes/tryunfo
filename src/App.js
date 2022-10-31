4a608d2bf92e03d84eb627cde1c69e74ae374ea9
Thaina-Abrantes <oliveiraabrantess@gmail.com>
2022-10-31 15:10:14 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
     this.state = {
       cardName: '',
       cardDescription: '',
-      cardAttr1: 0,
-      cardAttr2: 0,
-      cardAttr3: 0,
+      cardAttr1: '',
+      cardAttr2: '',
+      cardAttr3: '',
       cardImage: '',
       cardRare: 'normal',
       cardTrunfo: false,
@@ -19,17 +19,55 @@ class App extends Component {
     };
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.enableSaveButton = this.enableSaveButton.bind(this);
   }
 
-  onInputChange(event) {
+  enableSaveButton = () => {
+    const
+      {
+        cardName,
+        cardDescription,
+        cardImage,
+        cardRare,
+        cardAttr1,
+        cardAttr2,
+        cardAttr3,
+      } = this.state;
+
+    const attr1 = Number(cardAttr1);
+    const attr2 = Number(cardAttr2);
+    const attr3 = Number(cardAttr3);
+    const maxValue = 210;
+    const maxScore = 90;
+
+    if (cardName.length === 0
+        || cardDescription.length === 0
+        || cardImage.length === 0
+        || cardRare.length === 0
+        || (attr1 + attr2 + attr3) > maxValue
+        || (attr1 > maxScore || attr1 < 0)
+        || (attr2 > maxScore || attr2 < 0)
+        || (attr3 > maxScore || attr3 < 0)) {
+      this.setState({ isSaveButtonDisabled: true });
+    } else {
+      this.setState({ isSaveButtonDisabled: false });
+    }
+  };
+
+  onInputChange = (event) => {
     const { target } = event;
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
-    this.setState({
-      [name]: value,
-    });
+    this.setState(
+      {
+        [name]: value,
+      },
+      () => {
+        this.enableSaveButton();
+      },
+    );
     return value;
-  }
+  };
 
   render() {
     const {
